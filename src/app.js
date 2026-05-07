5cccd47742532aaa595df1ba3be0e97000a66519
Boudadi <anas.boudadi@etudiant.univ-reims.fr>
2026-05-07 17:28:39 +0200

    ok

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ const config = require('./config')
 const app = express()
 app.use(express.json())
 app.use('/', routes)
-
+app.get('/health', (req, res) => {
+  res.status(200).send('OK')
+})
 if (require.main === module) {
   app.listen(config.port, () => {
     console.log(`${config.appName} running on port ${config.port}`)
